5debfd9f880965bb1749c8ea986040757b3d6f5d
taskevich <93867297+taskevich@users.noreply.github.com>
2024-01-26 07:38:32 +0300

    Create 0756.cpp

---
+++ 0756.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << (a - 1) * (b - 1) << endl;
+
+    return 0;
+}
